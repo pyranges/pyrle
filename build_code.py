d5c2e8d508e256cd724b0d0cff506cfb838ec299
Endre Bakken Stovner <endrebak@Endres-MBP.lan>
2018-04-23 19:58:54 +0200

    Add div zeroes and nonzeroes

--- build_code.py
+++ build_code.py
@@ -7,11 +7,13 @@ j2_env = Environment(loader=FileSystemLoader("."),
                     trim_blocks=True)
 
 d_add_sub = yaml.load(open("template_filler_add_sub.yaml"))
+d_div = yaml.load(open("template_filler_div.yaml"))
 
 template = "rle_arithmetic_template.j2"
 
 code = j2_env.get_template(template).render(
     configs_add_sub=d_add_sub.values(),
+    configs_div=d_div.values(),
 )
 
 open("src/rle.pyx", "w+").write(code)
